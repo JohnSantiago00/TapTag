928016f997405976d1976637e2da97c2fee69de2
John Santiago <santiago.john96@gmail.com>
2025-11-27 14:34:07 -0500

    Login page

---
+++ tapTag/hooks/useAuthRedirect.ts
@@ -0,0 +1,21 @@
+import { useRouter, useSegments } from "expo-router";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { auth } from "../src/config/firebase";
+
+export function useAuthRedirect() {
+  const router = useRouter();
+  const segments = useSegments();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      const inAuthGroup = segments[0] === "(auth)";
+      if (user && inAuthGroup) {
+        router.replace("/(tabs)");
+      } else if (!user && !inAuthGroup) {
+        router.replace("/(auth)/Login");
+      }
+    });
+    return unsubscribe;
+  }, [segments]);
+}
