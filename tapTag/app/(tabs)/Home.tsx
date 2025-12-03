2e91eaf5cf5f068be0fd3f12bd48e9444148dba3
John Santiago <santiago.john96@gmail.com>
2025-12-02 20:24:14 -0500

    context

--- tapTag/app/(tabs)/Home.tsx
+++ tapTag/app/(tabs)/Home.tsx
@@ -74,7 +74,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000",
+    backgroundColor: "#a1a1a1",
     justifyContent: "center",
     alignItems: "center",
   },
