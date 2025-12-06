a261cdb9782a0a48fdde154017c0ebe2eebd5bf3
John Santiago <santiago.john96@gmail.com>
2025-12-06 13:41:32 -0500

    Cards Branch

--- tapTag/app/(tabs)/Home.tsx
+++ tapTag/app/(tabs)/Home.tsx
@@ -74,7 +74,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#a1a1a1",
+    backgroundColor: "#000",
     justifyContent: "center",
     alignItems: "center",
   },
